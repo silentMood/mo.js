190f3fc9a58617dd65772517dc65799df513ff41
kang.dai@baidao.com <dai@herbertdeMacBook-Air.local>
2015-05-19 17:21:28 +0800

    add the first test

---
+++ test/test_compile.js
@@ -0,0 +1,10 @@
+var assert = require("assert")
+
+describe('compile', function(){
+
+  it('should return code', function(){
+    assert.equal(-1, [1,2,3].indexOf(5));
+    assert.equal(-1, [1,2,3].indexOf(0));
+  });
+
+});
