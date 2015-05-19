96b7a10533498a621707133e0453df90cdbeac20
kang.dai@baidao.com <dai@herbertdeMacBook-Air.local>
2015-05-19 16:38:45 +0800

    fixed the compile module

--- src/compile.js
+++ src/compile.js
@@ -1,37 +1,58 @@
 var assert = require('./assert');
+var Scene = require('./scene');
+var sceneIdentifier = 'scene';
 var dPrefix = 'd-';
 
 function compile(el) {
 	assert(el !== null);
-
-	$nodes = [el];
-	while($nodes.length) {
-		$el = $nodes[0];
-		$childNodes = $el.childNodes;
-		if(!$childNodes.length) {
-			$el.shift();
-			continue;
-		}
-		for(var i = 0; i < $childNodes.length; i ++) {
-			$node = $childNodes[i];
-			//key link directive
-			LinkDirective($node);
-			$nodes.push($node);
-		}
-		$el.shift();
+	//may be other way , i totally have no idea on this
+	$scenes = document.querySelectorAll('.scene');
+	for(var i = 0; i < $scenes.length; i++) {
+		compileScenes($scenes[i]);
 	}
-
 }
 
-function LinkDirective(el) {
-	//need more assert
+function compileScenes(el) {
 	assert(el !== null);
 
 	attrs = el.attributes;
 	for(var i = 0; i < attrs.length; i++) {
 		attr = attrs.item(i);
+		scene = new Scene({sid: attr.value});
+		compileDirectives(el, scene);
+	}
+}
+
+function compileDirectives(el, scene) {
+	assert(el !== null);
+	assert(scene !== null);
+
+	$nodes = [el];
+	while($nodes.length) {
+		$el = $nodes[0];
+		linkDirectives($el, scene);
+		
+		$childNodes = $el.childNodes;
+		if(!$childNodes.length) {
+			$nodes.shift();
+			continue;
+		}
+		for(var i = 0; i < $childNodes.length; i++) {
+			$nodes.push($childNodes[i]);
+		}
+		$el.shift();
+	}	
+}
+
+function linkDirectives(el, scene) {
+	assert(el !== null);
+	assert(scene !== null);
+
+	attrs = el.attributes;
+	for(var i = 0; i < attrs.length; i++) {
+		attr = attrs.item(i);
 		if(attr.nodeName.match(dPrefix)) {
-			//link work
+			//todo compile directive
 		}
 	}
 }
